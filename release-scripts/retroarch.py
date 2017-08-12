069235b332277acd651c7c48860b21fd97571353
oxavelar <omar.avelar@gmail.com>
2017-08-12 13:24:29 -0500

    Let's see!

--- release-scripts/retroarch.py
+++ release-scripts/retroarch.py
@@ -16,15 +16,27 @@ os.environ['LD_LIBRARY_PATH'] = os.path.join(retro_dir, 'lib')
 
 retro_cmd = retro_bin + ' --config ' + retro_cfg
 retro_ref = 'python ' + retro_ref
-media_stop = 'systemctl stop kodi'
-media_start = 'systemctl start kodi'
+media_stop = 'pkill -SIGSTOP kodi.bin'
+media_start = 'systemctl -SIGCONT kodi.bin'
 
-try:
-    subprocess.call([retro_ref], shell=True)
-    os.system(media_stop)
-    subprocess.call([retro_cmd], shell=True)
-except KeyboardInterrupt:
-    pass
-finally:
-    os.system(media_start)
+
+def main():
+    try:
+        subprocess.call([retro_ref], shell=True)
+        os.system(media_stop)
+        subprocess.call([retro_cmd], shell=True)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        pass
+        os.system(media_start)
+
+
+if __name__ == '__main__':
+    main()
+else:
+    # Independent process of launched from within Kodi
+    # to allow us to stop kodi independently and keep
+    # running retroarch without the overhead
+    subprocess.Popen(['nohup' + os.path.abspath(__file__)], preexec_fn=os.setpgrp)
 
