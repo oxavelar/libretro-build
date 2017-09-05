9a6407fc9d166f757050149b87b31b96138f3684
oxavelar <omar.avelar@gmail.com>
2017-09-05 10:31:53 -0500

    Allows kodi to be stopped to save memory

--- release-scripts/retroarch.py
+++ release-scripts/retroarch.py
@@ -3,6 +3,7 @@
 
 import os
 import sys
+import time
 import subprocess
 
 path = os.path.dirname(__file__)
@@ -12,31 +13,52 @@ retro_bin = os.path.join(retro_dir, 'bin/retroarch')
 retro_cfg = os.path.join(retro_dir, 'config/retroarch.cfg')
 retro_ref = os.path.join(retro_dir, 'bin/retroarch-update-playlists.py')
 
-os.environ['LD_LIBRARY_PATH'] = os.path.join(retro_dir, 'lib')
 
-retro_cmd = retro_bin + ' --config ' + retro_cfg
-retro_ref = 'python ' + retro_ref
-media_stop = 'systemctl stop kodi'
-media_start = 'systemctl start kodi'
+def retroarch_update():
+    """ Will populate the retroarch playlists dynamically """
+    retro_cmd = 'python ' + retro_ref
+    subprocess.call([retro_cmd], shell=True)
+
+
+def retroarch_launch():
+    """ Executes our own breed of retroarch portable """
+    retro_cmd = retro_bin + ' --config ' + retro_cfg
+    os.environ['LD_LIBRARY_PATH'] = os.path.join(retro_dir, 'lib')
+    subprocess.call([retro_cmd], shell=True)
+
+
+def systemd_service_spawn():
+    """ Allows us to suspend Kodi by spawning as a systemd unit """
+    try:
+        import kodi
+        #systemd_cmd = 'systemd-run --scope ' + os.path.abspath(__file__)
+        systemd_cmd = 'systemd-run --unit=retroarch.service ' + os.path.abspath(__file__)
+        os.system(systemd_cmd)
+    except ImportError:
+       exit(-17)
 
 
 def main():
+    """ Steps that pauses the media center and executes retroarch """
+
+    media_pause = lambda: os.system('pkill -SIGSTOP kodi.bin')
+    media_start = lambda: os.system('pkill -SIGCONT kodi.bin')
+
     try:
-        subprocess.call([retro_ref], shell=True)
-        os.system(media_stop)
-        subprocess.call([retro_cmd], shell=True)
+        retroarch_update()
+        media_pause()
+        retroarch_launch()
     except KeyboardInterrupt:
         pass
     finally:
         pass
-        os.system(media_start)
+        media_start()
+        exit(0)
 
 
 if __name__ == '__main__':
-    main()
-else:
-    # Independent process of launched from within Kodi
-    # to allow us to stop kodi independently and keep
-    # running retroarch without the overhead
-    subprocess.Popen(['nohup' + os.path.abspath(__file__)], preexec_fn=os.setpgrp)
+    if '--systemd' in sys.argv:
+        systemd_service_spawn()
+    else:
+        main()
 
